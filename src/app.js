8b3af276fd9010e35c4183200206c94eb52033b7
Ave-Wat <wattsa2@carleton.edu>
2021-04-10 01:23:08 -0500

    added word limits and fixed contact section, various wording on homepage

---
+++ src/app.js
@@ -0,0 +1,40 @@
+function maxlength(element, maxvalue){
+    var wordCount = element.value.split(/[\s]+/).length;
+    if(wordCount > maxvalue){
+        var difference = wordCount - maxvalue;
+        alert("Sorry, you have input "+wordCount+" words into the " + "text area box you just completed. It can return no more than "+ maxvalue +" words. Please abbreviate " + "your text by at least "+difference+" words");
+        return false;
+    }
+}
+
+function minlength(element, minvalue){
+    var wordCount = element.value.split(/[\s]+/).length;
+    if(wordCount < minvalue){
+        var difference = minvalue - wordCount;
+        alert("Sorry, you have input "+wordCount+" words into the " + "text area box you just completed. It must contain "+ minvalue + " words. Please extend your answer by "+difference+" words");
+        return false;
+    }
+}
+
+function initialize(){
+    var form = document.getElementById("form");
+    var textarea1 = document.getElementById("lq1");
+    var textarea2 = document.getElementById("lq2");
+    form.onsubmit = function (event) {
+      maxlength(textarea1, 500);
+      maxlength(textarea2, 500);
+      minlength(textarea1, 250);
+      minlength(textarea2, 250);
+
+    };
+    textarea1.onchange = function (event) {
+      maxlength(textarea1, 500);
+      minlength(textarea1, 250);
+    };
+    textarea2.onchange = function (event) {
+      maxlength(textarea2, 500);
+      minlength(textarea2, 250);
+    };
+}
+
+window.onload = initialize;
