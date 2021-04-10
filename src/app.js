a9de30aa5558a2498745578f626d2d6555d49c4f
Ave-Wat <wattsa2@carleton.edu>
2021-04-10 02:38:59 -0500

    working mobile nav button

--- src/app.js
+++ src/app.js
@@ -17,24 +17,24 @@ function minlength(element, minvalue){
 }
 
 function mobileOpenNav() {
-  var links = document.getElementById("navbar");
+  var links = document.getElementById("navmenu");
   var closeButton = document.getElementById('close_icon');
   links.style.display = "block";
   closeButton.style.display = "block";
 }
 
 function mobileCloseNav() {
-  var links = document.getElementById("navbar");
+  var links = document.getElementById("navmenu");
   var closeButton = document.getElementById('close_icon');
   links.style.display = "none";
   closeButton.style.display = "none";
 }
 
 function initialize(){
-  /*var mobileOpenNavButton = document.getElementById('open_icon');
+  var mobileOpenNavButton = document.getElementById('open_icon');
   mobileOpenNavButton.onclick = mobileOpenNav;
   var mobileCloseNavButton = document.getElementById('close_icon');
-  mobileCloseNavButton.onclick = mobileCloseNav;*/
+  mobileCloseNavButton.onclick = mobileCloseNav;
 
   if (document.getElementsByTagName("body")[0].className == "application"){
     var form = document.getElementById("form");
