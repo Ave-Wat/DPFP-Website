d43a5334b309f617c074cdc1e461a0c02cf754b2
Avery Watts <wattsa2@carleton.edu>
2021-11-26 23:56:16 -0600

    higher level changes

--- app.py
+++ app.py
@@ -1,32 +1,32 @@
 import sys
 import flask
-from flask_mail import Mail, Message
 from flask import request
 import os
+import logging
 
 ########### Initializing Flask ###########
 app = flask.Flask(__name__, static_folder='src', template_folder='templates')
 
-########### The website routes ###########
-@app.route('/')
-def get_main_page():
-    return flask.render_template('index.html')
+app.logger.addHandler(logging.StreamHandler(sys.stdout))
+app.logger.setLevel(logging.ERROR)
 
-@app.route('/home')
+########### The website routes ###########
+
+@app.route('/')
 def home():
-    return flask.render_template('index.html')
+    return flask.render_template('index.html', title='Panorama Project')
 
 @app.route('/application')
 def application():
-    return flask.render_template("application.html")
+    return flask.render_template("application.html", title="Panorama Project Application")
 
 @app.route('/projects')
 def projects():
-    return flask.render_template("projects.html")
+    return flask.render_template("projects.html", title="Our Completed Projects")
 
-@app.route('/projectImmigration')
-def projectImmigration():
-    return flask.render_template("CarterGeorge_GabbyWatson_Final Draft.html")
+@app.route('/projects/<project_name>')
+def project(project_name):
+    return flask.render_template("{project}.html".format(project=project_name), title=project_name)
 
 @app.route('/<path:path>')
 def shared_header_catchall(path):
